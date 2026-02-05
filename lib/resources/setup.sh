51f89904a406f6cbffd6d3b995d26dfdf96996a6
yamamo-chi <ymkh.personal@gmail.com>
2026-02-05 23:00:11 +0900

    acme.shをec2-user経由せずに処理簡素化

--- lib/resources/setup.sh
+++ lib/resources/setup.sh
@@ -25,23 +25,23 @@ mkdir -p /usr/libexec/docker/cli-plugins
 curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/libexec/docker/cli-plugins/docker-compose
 chmod +x /usr/libexec/docker/cli-plugins/docker-compose
 
-# ec2-userにacme.shインストール
-su - ec2-user -c curl "https://get.acme.sh | sh"
+# acme.shインストール
+HOME=/root
+curl https://get.acme.sh | sh
+source ~/.acme.sh/acme.sh.env
 
 # パブリックIP取得
 PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
 
 # IPアドレス証明書発行
-ACME_DIR="/home/ec2-user/.acme.sh"
-$ACME_DIR/acme.sh --issue \
+acme.sh --issue \
   --standalone \
   --httpport 80 \
   --listen-v4 \
   -d $PUBLIC_IP \
   --server letsencrypt \
   --cert-profile shortlived \
-  --days 3 \
-  --home $ACME_DIR
+  --days 3
 
 # Docker Compose 起動
 cd $SCRIPT_DIR
@@ -49,12 +49,7 @@ docker compose up -d
 
 # NginxへIPアドレス証明書のインストールと自動更新設定
 mkdir -p /home/ec2-user/openwebui/nginx/ssl
-$ACME_DIR/acme.sh --install-cert -d $PUBLIC_IP \
+acme.sh --install-cert -d $PUBLIC_IP \
 --key-file /home/ec2-user/openwebui/nginx/ssl/key.pem \
 --fullchain-file /home/ec2-user/openwebui/nginx/ssl/fullchain.pem \
---reloadcmd "docker exec nginx-proxy nginx -s reload" \
---home $ACME_DIR
-
-# 権限をrootからec2-userに変更
-chown -R ec2-user:ec2-user /home/ec2-user/openwebui/nginx/ssl
-chown -R ec2-user:ec2-user /home/ec2-user/.acme.sh
+--reloadcmd "chown ec2-user:ec2-user $SCRIPT_DIR/nginx/ssl/*.pem && docker exec nginx-proxy nginx -s reload" \
