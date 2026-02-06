13c88d4136158ca649cf25c64edc5e6dbea647f6
yamamo-chi <ymkh.personal@gmail.com>
2026-02-07 00:28:39 +0900

    acme.sh実行失敗修正

--- lib/resources/setup.sh
+++ lib/resources/setup.sh
@@ -26,15 +26,14 @@ curl -SL https://github.com/docker/compose/releases/latest/download/docker-compo
 chmod +x /usr/libexec/docker/cli-plugins/docker-compose
 
 # acme.shインストール
-HOME=/root
-curl https://get.acme.sh | sh
-source ~/.acme.sh/acme.sh.env
+curl https://get.acme.sh | HOME=/root sh
+ACME_DIR=/root/.acme.sh
 
 # パブリックIP取得
 PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
 
 # IPアドレス証明書発行
-acme.sh --issue \
+$ACME_DIR/acme.sh --issue \
   --standalone \
   --httpport 80 \
   --listen-v4 \
@@ -49,7 +48,7 @@ docker compose up -d
 
 # NginxへIPアドレス証明書のインストールと自動更新設定
 mkdir -p /home/ec2-user/openwebui/nginx/ssl
-acme.sh --install-cert -d $PUBLIC_IP \
+$ACME_DIR/acme.sh --install-cert -d $PUBLIC_IP \
 --key-file /home/ec2-user/openwebui/nginx/ssl/key.pem \
 --fullchain-file /home/ec2-user/openwebui/nginx/ssl/fullchain.pem \
 --reloadcmd "chown ec2-user:ec2-user $SCRIPT_DIR/nginx/ssl/*.pem && docker exec nginx-proxy nginx -s reload" \
